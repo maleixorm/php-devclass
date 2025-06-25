8b250514ebc03e34be094b87e7d8bafe010ea67a
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-25 14:06:07 -0300

    Criando a Index do projeto

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<? require "../app/functions/pages.php"; ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP Devclass</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <?php require load(); ?>
+    </div>
+</body>
+</html>
