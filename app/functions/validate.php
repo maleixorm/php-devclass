6a1cc41ac5aa7c7d00ffa20c160fe27c555f38ce
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-25 20:46:46 -0300

    Criando a função para validar os dados recebido pelo formlário

---
+++ app/functions/validate.php
@@ -0,0 +1,19 @@
+<?php
+
+function validate(array $fields) {
+    $validate = [];
+    foreach ($fields as $field => $type) {
+        switch ($type) {
+            case 's':
+                $validate[$field] = strip_tags($_POST[$field]);
+                break;
+            case 'i':
+                $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_NUMBER_INT);
+                break;
+            case 'e':
+                $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_EMAIL);
+                break;
+        }
+    }
+    return (object) $validate;
+}
