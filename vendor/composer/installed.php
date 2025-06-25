f71e429e6285da21eef50740193babd238a242fc
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-25 20:41:46 -0300

    Adicionando o autoload através do composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '64a6c07b4077450b7e83b834b27ccf73d45d0dcb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '64a6c07b4077450b7e83b834b27ccf73d45d0dcb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
