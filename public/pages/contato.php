0f2d170c2a7b306296ab66f0862597544bdb94a6
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-25 14:33:13 -0300

    Criando o formulário de contato

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -1,1 +1,23 @@
 <h2>Contato</h2>
+
+<form action="pages/forms/contato.php" method="post" role="form">
+
+    <div class="form-group">
+        <label for="name" class="form-label">Nome:</label>
+        <input type="text" name="name" id="name" class="form-control" placeholder="Digite seu nome...">
+    </div>
+    <div class="form-group">
+        <label for="email" class="form-label">Email:</label>
+        <input type="email" name="email" id="email" class="form-control" placeholder="Digite seu email...">
+    </div>
+    <div class="form-group">
+        <label for="subject" class="form-label">Assunto:</label>
+        <input type="text" name="subject" id="subject" class="form-control" placeholder="Digite o assunto...">
+    </div>
+    <div class="form-group">
+        <label for="message" class="form-label">Mensagem:</label>
+        <textarea name="message" id="message" class="form-control" cols="30" rows="10" placeholder="Sua mensagem aqui..."></textarea>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
