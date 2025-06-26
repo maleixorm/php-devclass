b34344f11ca426548bdbc6658f03975a595e917d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-26 11:31:57 -0300

    Create function isEmpty to validate blank fields on form

--- app/functions/validate.php
+++ app/functions/validate.php
@@ -18,3 +18,14 @@ function validate(array $fields) {
     }
     return (object) $validate;
 }
+
+function isEmpty() {
+    $request = request();
+    $empty = false;
+    foreach ($request as $key => $value) {
+        if(empty($request[$key])) {
+            $empty = true;
+        }
+    }
+    return $empty;
+}
