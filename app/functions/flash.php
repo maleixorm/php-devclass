8e0ce5612b7b75b6677c84fc711c8c98f1d2d905
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-26 11:43:56 -0300

    update tag on message interface

--- app/functions/flash.php
+++ app/functions/flash.php
@@ -2,7 +2,7 @@
 
 function flash($key, $message, $type = 'danger') {
     if (!isset($_SESSION['flash'][$key])) {
-        $_SESSION['flash'][$key] = "<span class='alert alert-{$type}'>".$message."</span>";
+        $_SESSION['flash'][$key] = "<div class='alert alert-{$type}'>".$message."</div>";
     }
 }
 
