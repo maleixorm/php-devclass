e07a2e06dc723feea02e9aa13f1401b0ce2fc81f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-26 10:41:31 -0300

    Criando a função request, para identificar o tipo de método da requisição

--- app/functions/custom.php
+++ app/functions/custom.php
@@ -5,3 +5,11 @@ function dd($dump) {
     var_dump($dump);
     die();
 }
+
+function request() {
+    $request = $_SERVER['REQUEST_METHOD'];
+    if ($request == 'POST') {
+        return $_POST;
+    }
+    return $_GET;
+}
