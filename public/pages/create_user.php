7cd88266deb72bcbdcff8745a0838382cda9b81f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-27 17:44:47 -0300

    add create user page

---
+++ public/pages/create_user.php
@@ -0,0 +1,27 @@
+<form action="" method="post" role="form">
+    
+    <div class="form-group">
+        <label for="name" class="form-label">Nome: </label>
+        <input type="text" name="name" id="name" class="form-control" placeholder="Seu nome aqui..." />
+    </div>
+    
+    <div class="form-group">
+        <label for="lastname" class="form-label">Sobrenome: </label>
+        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Seu sobrenome aqui..." />
+    </div>
+    
+    <div class="form-group">
+        <label for="email" class="form-label">Email: </label>
+        <input type="email" name="email" id="email" class="form-control" placeholder="Seu sobrenome aqui..." />
+    </div>
+    
+    <div class="form-group">
+        <label for="password" class="form-label">Senha: </label>
+        <input type="password" name="password" id="password" class="form-control" placeholder="Seu sobrenome aqui..." />
+    </div>
+
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
+    
+    
+</form>
+
--- public/pages/home.php
+++ public/pages/home.php
@@ -1,1 +1,3 @@
+<a href="?page=create_user">Cadastrar Usuário</a>
+
 <h2>Página Inicial</h2>
