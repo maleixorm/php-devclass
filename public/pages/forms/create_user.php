64072a97b75191e64c89e17efd7120f73bc431b5
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-27 18:44:22 -0300

    Criando a validação para o cadastro de usuários

---
+++ public/pages/forms/create_user.php
@@ -0,0 +1,25 @@
+<?php
+
+require "../../../bootstrap.php";
+
+if (isEmpty()) {
+    flash('message', 'Preencha todos os campos do formulário.');
+    redirect("contato");
+}
+
+$validate = validate([
+    'name' => 's',
+    'lastname' => 's',
+    'email' => 'e',
+    'password' => 's'
+]);
+
+$cadastrado = create('users', $validate);
+
+if ($cadastrado) {
+    flash('message', 'Usuário cadastrado com sucesso!', 'success');
+    return redirect('create_user');
+}
+
+flash('message', 'Erro ao cadastrar usuário!');
+redirect('create_user');
