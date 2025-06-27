4a66a525746bd961de978205668e61741021d31c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-27 20:28:28 -0300

    Corrigindo a conexão e criando a função de cadastro de usuários

--- app/functions/database.php
+++ app/functions/database.php
@@ -1,14 +1,25 @@
 <?php
 
 function connect() {
-    $pdo = new PDO("mysql:host=localhost;dbname=fullstack;charset=utf8, 'php', '123456'");
+    $pdo = new PDO("mysql:host=localhost;dbname=fullstack;charset=utf8", 'php', '123456');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     return $pdo;
 }
 
-function create() {
+function create($table, $fields) {
+    if (!is_array($fields)) {
+        $fields = (array) $fields;
+    }
+    $sql = "INSERT INTO {$table}";
+    $sql .= "(". implode(',', array_keys($fields)) . ")";
+    $sql .= " values (:" . implode(',:', array_keys($fields)) . ");";
 
+    // dd($sql);
+
+    $pdo = connect();
+    $insert = $pdo->prepare($sql);
+    return $insert->execute($fields);
 }
 
 function update() {
