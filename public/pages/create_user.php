afc1e8952f6940ebee606d3903cef9195dd2d605
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-28 01:44:00 -0300

    changing placeolders

--- public/pages/create_user.php
+++ public/pages/create_user.php
@@ -2,22 +2,22 @@
     
     <div class="form-group">
         <label for="name" class="form-label">Nome: </label>
-        <input type="text" name="name" id="name" class="form-control" placeholder="Seu nome aqui..." />
+        <input type="text" name="name" id="name" class="form-control" placeholder="Seu nome..." />
     </div>
     
     <div class="form-group">
         <label for="lastname" class="form-label">Sobrenome: </label>
-        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Seu sobrenome aqui..." />
+        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Seu sobrenome..." />
     </div>
     
     <div class="form-group">
         <label for="email" class="form-label">Email: </label>
-        <input type="email" name="email" id="email" class="form-control" placeholder="Seu sobrenome aqui..." />
+        <input type="email" name="email" id="email" class="form-control" placeholder="Seu email..." />
     </div>
     
     <div class="form-group">
         <label for="password" class="form-label">Senha: </label>
-        <input type="password" name="password" id="password" class="form-control" placeholder="Seu sobrenome aqui..." />
+        <input type="password" name="password" id="password" class="form-control" placeholder="Sua senha..." />
     </div>
 
     <button type="submit" class="btn btn-primary">Cadastrar</button>
